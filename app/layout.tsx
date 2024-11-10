fa0b5233cced805a04ca67564434b61b599ff26a
liuxzzz <691381755@qq.com>
2024-11-11 01:05:54 +0800

    feat: update metaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,9 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
 
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
@@ -16,7 +13,7 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   icons: {
-    icon: "/favicon.ico",
+    icon: "/logo.ico",
   },
 };
 
